dd83b723a0b35b3cf2166ea8d2bef58e6690bc9c
SAI MEGHANA ANNEPU <157686141+Maggiee18@users.noreply.github.com>
2024-02-05 11:39:13 +0530

    Add files via upload

---
+++ Sum of all elements of array.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+	
+	int n,sum=0;
+	printf("Enter the number of elements: ");
+	scanf("%d", &n);
+	
+	int arr[n];
+	int i;
+	printf("Enter %d elements: \n");
+	for(i=0;i<n;i++){
+		scanf("%d", &arr[i]);
+		sum+=arr[i];
+	}
+	printf("Sum of the elements is %d", sum);
+	
+	return 0;
+	
+}
