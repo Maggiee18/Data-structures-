415287a77dd9f5e83fdb325b5aa856fa2cdf8acc
SAI MEGHANA ANNEPU <157686141+Maggiee18@users.noreply.github.com>
2024-02-10 23:23:12 +0530

    Add files via upload

---
+++ Implementing binary search.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,search, first, last, middle;
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+    
+    int arr[n];
+    printf("Enter %d elements (in ascending order): ", n);
+    for(i=0; i<10; i++)
+        scanf("%d", &arr[i]);
+        
+    printf("Enter the element you want to search: \n");
+    scanf("%d", &search);
+    first = 0;
+    last = 9;
+    middle = (first+last)/2;
+    
+    while(first <= last)
+    {
+        if(arr[middle]<search)
+            first = middle+1;
+        else if(arr[middle]==search)
+        {
+            printf("\n%d found at Position %d", search, middle+1);
+            break;
+        }
+        else
+            last = middle-1;
+        middle = (first+last)/2;
+    }
+    
+    if(first>last)
+        printf("\n%d is not found in the given Array", search);
+
+    return 0;
+}
